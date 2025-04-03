b12be8650e3ac3c0062a7a85f106affdeb9a3fa8
Isabellabmj <Isabellajoensen@gmail.com>
2025-04-03 11:57:52 +0200

    Start

---
+++ src/main/java/com/example/danmarkmodmadspild2/DanmarkModMadspild2Application.java
@@ -0,0 +1,15 @@
+package com.example.danmarkmodmadspild2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanmarkModMadspild2Application
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(DanmarkModMadspild2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/danmarkmodmadspild2/DanmarkModMadspild2ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.danmarkmodmadspild2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanmarkModMadspild2ApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
