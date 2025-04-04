f191362089c2c630160891cccfbb391c05c80738
MikkelRosenqvist <miha0016@stud.kea.dk>
2025-04-04 11:49:01 +0200

    virksomhed funktionalitet

--- src/main/java/com/example/danmarkmodmadspild2/Controller/HomeController.java
+++ src/main/java/com/example/danmarkmodmadspild2/Controller/HomeController.java
@@ -3,12 +3,37 @@ package com.example.danmarkmodmadspild2.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String index(Model model)
-    {
+    public String index(Model model) {
         return "home/index";
+    }
+
+    @GetMapping("/virksomhed")
+    public String showVirksomhedPage() {
+        return "home/virksomhed";
+    }
+
+    @GetMapping("/signup")
+    public String showSignupPage() {
+        return "home/virksomhed-signup";
+    }
+
+    @PostMapping("/signup")
+    public String handleSignup(@RequestParam String companyName,
+                               @RequestParam String email,
+                               @RequestParam String location,
+                               @RequestParam String password) {
+        // signup validering
+        return "redirect:/home/login"; // omdirigerer til login ved signup succes
+    }
+
+    @GetMapping("/login")
+    public String showLoginPage() {
+        return "home/virksomhed-login";
     }
 }
